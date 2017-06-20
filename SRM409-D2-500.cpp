7e2b8e3cbd0bc92d67b8f8d2f7aabf6b7a37c741
AmrMorsySuperVision <amrmorsy2@hotmail.com>
2017-06-20 21:18:02 +0200

    Add files via upload

---
+++ SRM409-D2-500.cpp
@@ -0,0 +1,70 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+class OrderedSuperString
+{
+public:
+    int getLength(vector <string> words)
+    {
+        string s="";
+        int last=0;
+        for(int i=0; i<words.size(); i++)
+        {
+            bool check2=0;
+            for(int j=last; j<s.size(); j++)
+            {
+                if(s[j]==words[i][0])
+                {
+                    int ind=j;
+                    bool check=1;
+                    for(int f=0; f<words[i].size() && ind<s.size() ; f++,ind++)
+                        if(words[i][f]!=s[ind])
+                        {
+                            check=0;
+                            break;
+                        }
+                    if(check)
+                    {
+                        check2=1;
+                        last=j;
+                        break;
+                    }
+                }
+            }
+            if(!check2)
+                last=s.size();
+            int ind=last;
+            for(int j=0;j<words[i].size();j++)
+            {
+                if(ind>=s.size())
+                {
+                    ind++;
+                    s+=words[i][j];
+                }
+                else
+                    ind++;
+            }
+        }
+        return s.size();
+    }
+};
