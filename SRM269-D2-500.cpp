e8f0fdc7eee69dfa1b4d955d3242819b152e39c1
AmrMorsySuperVision <amrmorsy2@hotmail.com>
2017-06-21 00:01:25 +0200

    Add files via upload

---
+++ SRM269-D2-500.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+class PatternOptimizer {
+public:
+	string optimize(string pattern) {
+		pattern+='a';
+		string op="";
+		for(int i=0;i<pattern.size();i++)
+        {
+            if(isalpha(pattern[i]))
+                op+=pattern[i];
+            else
+            {
+                int cnt=0;
+                bool check=0;
+                for(i;i<pattern.size();i++)
+                {
+                    if(isalpha(pattern[i]))
+                        break;
+                    if(pattern[i]=='?')
+                        cnt++;
+                    else if(pattern[i]=='*')
+                        check=1;
+                }
+                i--;
+                if(check)
+                    op+='*';
+                for(int j=0;j<cnt;j++)
+                    op+='?';
+            }
+        }
+        op.pop_back();
+        return op;
+	}
+};
