084310f2cb2f285188201652930660e9ef08ecf3
AmrMorsySuperVision <amrmorsy2@hotmail.com>
2017-07-07 09:37:41 +0200

    Add files via upload

---
+++ LIVEARCHIVE 2935.cpp
@@ -0,0 +1,56 @@
+/*
+	- Using a tree property that there's a uniqe path between each 2 nodes
+	- For each subtree find the string the represt it after that recall the function to find the smallest string in the current sub tree
+
+*/
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string re(string a)
+{
+    if(a.size()==0)
+        return "";
+    string tmp="";
+    vector<string> v;
+    int cnt=0;
+    for(int i=0;i<a.size();i++)
+    {
+        tmp+=a[i];
+        if(a[i]=='0')
+            cnt++;
+        else
+            cnt--;
+        if(!cnt)
+        {
+            tmp=tmp.substr(1,tmp.size()-2);
+            v.push_back("0"+re(tmp)+"1");
+            tmp="";
+        }
+    }
+    sort(v.begin(),v.end());
+    tmp="";
+    for(int i=0;i<v.size();i++)
+        tmp+=v[i];
+    return tmp;
+}
+
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    string a,b;
+    while(tc--)
+    {
+        cin>>a>>b;
+        //cout<<re(a)<<endl<<re(b)<<endl;
+        if(re(a)==re(b))
+            cout<<"same"<<endl;
+        else
+            cout<<"different"<<endl;
+    }
+    return 0;
+}
