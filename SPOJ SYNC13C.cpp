aea8102f45ed8dfbe13346e4b8ee12acb13de58e
AmrMorsySuperVision <amrmorsy2@hotmail.com>
2017-09-12 23:11:32 +0200

    Add files via upload

---
+++ SPOJ SYNC13C.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        int w,b;
+        cin>>w>>b;
+        cout<<((b%2)?"1.000000":"0.000000")<<endl;
+    }
+    return 0;
+}
